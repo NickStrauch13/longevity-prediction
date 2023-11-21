a3ed8a5587222f2010ab2cb47654de57e922a829
mrinoybanerjee <mrinoybanerjee@gmail.com>
2023-11-20 23:20:47 -0500

    data modeling script ready

--- src/main.py
+++ src/main.py
@@ -11,8 +11,12 @@ def main():
         country_name = str(sys.argv[2]).lower().title().strip()
 
         print("Country Name: ", country_name)
-        print("Life Expectancy: ", predict_life_expectancy(country_name))
-
-
+        life_expectancy, top_rf_features, top_z_score_features = predict_life_expectancy(country_name)
+        print("Life Expectancy: ", life_expectancy)
+        print("Top RF Features: ", top_rf_features)
+        print("Top Z-Score Features: ", top_z_score_features)
+    else:
+        print("Invalid option. Please enter either 'train' or 'predict'.")
+        sys.exit(1)
 if __name__ == "__main__":
     main()
--- src/modeling.py
+++ src/modeling.py
@@ -12,6 +12,7 @@ import mysql.connector
 from mysql.connector import Error
 import joblib
 import matplotlib.pyplot as plt
+from util import fetch_data_from_db
 
 
 def source_data():
@@ -20,6 +21,11 @@ def source_data():
 
 
 def train_rf_model(data):
+    '''
+    This function trains a Random Forest Regressor model on the data and returns
+    the model and the predictions.
+    '''
+    # dropping columns with bad data
     data.drop(columns=["Measles", "Polio",
               "Literacy rate, youth total"], inplace=True)
 
@@ -50,46 +56,54 @@ def train_rf_model(data):
     rf_model.fit(X_train, y_train)
     preds = rf_model.predict(X_test)
 
+    # Save the model
+    save_pickled_model(rf_model)
+
     return rf_model, preds
 
 
 # Function to predict life expectancy and top 3 influencing features for a given country
 def predict_life_expectancy(country_name):
+    '''
+    This function returns the predicted life expectancy for a given country
+    and the top 3 features that influence the prediction.
+    '''
     file_path = '../data/Cleaned/longevity_reduced.csv'
     data = pd.read_csv(file_path)
     target = 'Life expectancy at birth, total (years)'
-
     # Find the row in the dataframe that corresponds to the given country
     country_data = data[data['Country Name'] == country_name].drop(
         columns=[target, 'Country Name'])
 
-    # Predict life expectancy using the trained model
-    rf_model = joblib.load('../models/rf_model.joblib')
-    life_expectancy = rf_model.predict(country_data)
-
-    # Calculate the deviation of the country's feature values from the dataset mean
-    deviations = country_data.squeeze() - X.mean()
-
-    # Determine if the deviation is higher or lower than the mean
-    deviation_direction = deviations.apply(
-        lambda x: 'higher' if x > 0 else 'lower')
-
-    # Combine the absolute deviation and its direction
-    deviations_with_direction = deviations.abs().sort_values(ascending=False).head(5)
-    deviations_with_direction = deviations_with_direction.to_frame(
-        name='Deviation')
-    deviations_with_direction['Direction'] = deviation_direction.loc[deviations_with_direction.index]
-
-    top_z_score_features = find_top_z_score_features(country_name)
-
-    return life_expectancy[0], deviations_with_direction, top_z_score_features
+    life_expectancy = country_data[target]
+    top_z_score_features = find_top_z_score_features(data, target, country_name)
+    top_rf_features = rf_feature_importance(country_data)
+    
+    return life_expectancy, top_rf_features, top_z_score_features
     # Function to find the top features based on Z-scores alone for the United States
 
+def rf_feature_importance(country_data):
+    '''
+    This function returns the top 3 features for a given country based on the feature
+    importances of the Random Forest model.
+    '''
+    # Get feature importances specific to the country's data
+    rf_model = load_pickled_model()
+    importances = rf_model.feature_importances_
+    indices = np.argsort(importances)[-5:]  # Get indices of top 3 features
+    top_features = country_data.columns[indices].tolist()  # Get names of top 3 features
+    # return if the deviation is higher or lower than the mean
+    return top_features
 
-def find_top_z_score_features(country_name):
+def find_top_z_score_features(data, target, country_name):
+    '''
+    This function returns the top 3 features for a given country based on the Z-scores
+    of the features.
+    '''
     # Isolate the row for the given country
     country_row = data[data['Country Name'] == country_name].drop(
         columns=[target, 'Country Name'])
+    X = data.drop(columns=[target, 'Country Name'])
 
     # Calculate the mean and std deviation for the features
     mean_values = X.mean()
@@ -116,7 +130,11 @@ def find_top_z_score_features(country_name):
     return top_z_scores
 
 
-def get_country_name_conversions():
+def get_country_name_conversions(data):
+    '''
+    This function returns a dictionary of country names from the world atlas
+    and the corresponding country names from the World Bank data.
+    '''
     url = "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json"
     # Get json from url
     r = requests.get(url)
@@ -146,9 +164,21 @@ def get_country_name_conversions():
                 print(f"{c} ---> {match}")
                 conversion_dict[c] = None
                 z += 1
-
         for key in conversion_dict.keys():
             if conversion_dict[key] and conversion_dict[key] != key:
                 print(f"Key: {key}, Value: {conversion_dict[key]}")
-
         return conversion_dict
+
+
+def save_pickled_model(model):
+    '''
+    This function saves the model as a pickle file.
+    '''
+    joblib.dump(model, '../models/rf_model.joblib')
+
+def load_pickled_model():
+    '''
+    This function loads the pickle file and returns the model.
+    '''
+    model = joblib.load('../models/rf_model.joblib')
+    return model
